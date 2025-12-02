c4bda04654f3af19d5af02029255fd268003d9cb
rulos26 <rulos26@gmail.com>
2025-12-02 08:11:10 -0500

    Update database connection settings in config.php to change credentials for user and database name, ensuring compatibility with the new database setup. Commented out previous settings for clarity.

--- config.php
+++ config.php
@@ -7,11 +7,17 @@
 // Configuración de la base de datos
 // NOTA: En Hostinger, cuando el script PHP está en el mismo servidor que la BD,
 // usa '127.0.0.1' en lugar de la IP externa
-define('DB_HOST', '127.0.0.1');
+/* define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
 define('DB_USER', 'u494150416_ZLlua');
 define('DB_PASS', '!$a2-b2}^Kf.!Gj0');
-define('DB_NAME', 'u494150416_LilLT');
+define('DB_NAME', 'u494150416_LilLT'); */
+
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+define('DB_USER', 'u494150416_gatita');
+define('DB_PASS', '0382646740Ju*');
+define('DB_NAME', 'u494150416_gatita');
 
 /**
  * Función para obtener la conexión a la base de datos
